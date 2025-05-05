1243e99844fe67150fe6cccd03d04eb31291d6bb
Justin Lam <justinflam9225k@gmail.com>
2025-05-04 17:29:57 -0700

    Implemented tile suggestion
    For houses it's recommended to make a singular 6x3 house in the middle of the map
    
    For forests you can do whatever you want. Go ham

--- src/3_Generators/generateHouse.js
+++ src/3_Generators/generateHouse.js
@@ -9,8 +9,21 @@ const model = new WFCModel().learn(IMAGES.HOUSES, 2);
  */
 export default function generateHouse(boundingBox) {
   model.clearSetTiles();
-  model.setTile(0, 0, 53)	// orange roof TL corner
-  model.setTile(0, boundingBox.height - 1, 77); // blue house BL corner
+
+  model.setTile(0, boundingBox.height-1, 73);	// brown house BL corner
+  model.setTile(boundingBox.width-1, boundingBox.height-1, 76)	// brown house BR corner
+
+  //model.setTile(0, boundingBox.height-1, 77);	// blue house BL corner
+  //model.setTile(boundingBox.width-1, boundingBox.height-1, 80)	// blue house BR corner
+
+  model.setTile(0, 0, 49)	// blue roof TL corner
+  //model.setTile(boundingBox.width-1, 0, 51)	// blue roof TR corner
+  //model.setTile(boundingBox.width-1, 0, 52)	// blue roof TR chimney
+
+  //model.setTile(0, 0, 53)	// orange roof TL corner
+  //model.setTile(boundingBox.width-1, 0, 55)	// orange roof TR corner
+  //model.setTile(boundingBox.width-1, 0, 56)	// orange roof TRchimney
+
   const house = model.generate(boundingBox.width, boundingBox.height, 10, false, false);
   if (!house) throw new Error("Contradiction created");
   return house;
