a62a3b4dbc4be058e004dec23cd5f6bc8c3c2c0f
Justin Lam <justinflam9225k@gmail.com>
2025-05-08 17:25:25 -0700

    Refactored code

--- src/4_Phaser/Autotiler.js
+++ src/4_Phaser/Autotiler.js
@@ -29,22 +29,11 @@ export default class Autotiler extends Phaser.Scene {
     this.structsModel.learn(IMAGES.STRUCTURES, 2);
 
     window.addEventListener("generate", (e) => {
-      const groundMapData = this.groundModel.generate(TILEMAP.WIDTH, TILEMAP.HEIGHT, 10, false, false);
-      if (!groundMapData) throw new Error("Contradiction created");
-      
-      if (this.groundMap) this.groundMap.destroy();
-      this.groundMap = this.make.tilemap({
-        data: groundMapData,
-        tileWidth: TILEMAP.TILE_WIDTH,
-        tileHeight: TILEMAP.TILE_WIDTH
-      });
-      this.groundMap.createLayer(0, this.tileset, 0, 0);
-
-      const structsMapData_Sketch = [];
+      const sketchImage = [];
       for (let y = 0; y < TILEMAP.HEIGHT; y++) {
-        structsMapData_Sketch[y] = [];
+        sketchImage[y] = [];
         for (let x = 0; x < TILEMAP.WIDTH; x++) {
-          structsMapData_Sketch[y][x] = -1;
+          sketchImage[y][x] = -1;
         }
       }
 
@@ -62,7 +51,7 @@ export default class Autotiler extends Phaser.Scene {
             for (let x = 0; x < boundingBox.width; x++) {
               const dy = y + boundingBox.topLeft.y;
               const dx = x + boundingBox.topLeft.x;
-              structsMapData_Sketch[dy][dx] = struct[y][x];
+              sketchImage[dy][dx] = struct[y][x];
               this.structsModel.setTile(dx, dy, [struct[y][x]]);
             }}
           }
@@ -74,26 +63,47 @@ export default class Autotiler extends Phaser.Scene {
         }
       }
 
-      const structsMapData_WFC = this.structsModel.generate(TILEMAP.WIDTH, TILEMAP.HEIGHT, 10, true, true);
-      if (!structsMapData_WFC) throw new Error ("Contradiction created");
-
-      if (this.structsMap_Sketch) this.structsMap_Sketch.destroy();
-      this.structsMap_Sketch = this.make.tilemap({
-        data: structsMapData_Sketch,
-        tileWidth: TILEMAP.TILE_WIDTH,
-        tileHeight: TILEMAP.TILE_WIDTH
-      });
-      this.structsMap_Sketch.createLayer(0, this.tileset, 0, 0);
-
-      if (this.structsMap_WFC) this.structsMap_WFC.destroy();
-      this.structsMap_WFC = this.make.tilemap({
-        data: structsMapData_WFC,
-        tileWidth: TILEMAP.TILE_WIDTH,
-        tileHeight: TILEMAP.TILE_WIDTH
-      });
-      this.structsMap_WFC.createLayer(0, this.tileset, 0, 0).setAlpha(SUGGESTED_TILE_ALPHA);
+      this.createGroundMap()
+      this.createStructsMap_WFC();
+      this.createStructsMap_Sketch(sketchImage);
 
       console.log("Generation Complete");
     });
+  }
+
+  createGroundMap() {
+      const image = this.groundModel.generate(TILEMAP.WIDTH, TILEMAP.HEIGHT, 10, false, false);
+      if (!image) throw new Error("Contradiction created");
+      
+      if (this.groundMap) this.groundMap.destroy();
+      this.groundMap = this.make.tilemap({
+        data: image,
+        tileWidth: TILEMAP.TILE_WIDTH,
+        tileHeight: TILEMAP.TILE_WIDTH
+      });
+      this.groundMap.createLayer(0, this.tileset, 0, 0);
+  }
+
+  createStructsMap_WFC() {
+    const image = this.structsModel.generate(TILEMAP.WIDTH, TILEMAP.HEIGHT, 10, true, true);
+    if (!image) throw new Error ("Contradiction created");
+
+    if (this.structsMap_WFC) this.structsMap_WFC.destroy();
+    this.structsMap_WFC = this.make.tilemap({
+      data: image,
+      tileWidth: TILEMAP.TILE_WIDTH,
+      tileHeight: TILEMAP.TILE_WIDTH
+    });
+    this.structsMap_WFC.createLayer(0, this.tileset, 0, 0).setAlpha(SUGGESTED_TILE_ALPHA);
+  }
+
+  createStructsMap_Sketch(data) {
+    if (this.structsMap_Sketch) this.structsMap_Sketch.destroy();
+    this.structsMap_Sketch = this.make.tilemap({
+      data: data,
+      tileWidth: TILEMAP.TILE_WIDTH,
+      tileHeight: TILEMAP.TILE_WIDTH
+    });
+    this.structsMap_Sketch.createLayer(0, this.tileset, 0, 0);
   }
 }
