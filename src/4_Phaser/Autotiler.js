8d20d9c2167113228254f587d812f6d43351678f
corvuscorae <corvuscorae@gmail.com>
2025-05-14 21:50:37 -0700

    call clearSetTiles() on clear event

--- src/4_Phaser/Autotiler.js
+++ src/4_Phaser/Autotiler.js
@@ -79,6 +79,11 @@ export default class Autotiler extends Phaser.Scene {
 
       console.log("Generation Complete");
     });
+
+    window.addEventListener("clearSketch", (e) => {
+      this.groundModel.clearSetTiles();
+      this.structsModel.clearSetTiles();
+    });
   }
 
   // calls generators
