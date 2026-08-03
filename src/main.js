db379fe7b2d1a298fa9b6328eb88539c02b2ca32
Raven Ruiz <102258919+corvuscorae@users.noreply.github.com>
2026-08-03 18:26:27 -0400

    add embed mode

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import initSketchpad from "./1_Sketchpad/initSketchpad.js";
 import initPhaser from "./4_Phaser/initPhaser.js";
 
+const params = new URLSearchParams(window.location.search);
+
+if (params.get("embed") === "1") {
+	document.body.classList.add("embed-mode");
+}
+
 initSketchpad();
 initPhaser();
